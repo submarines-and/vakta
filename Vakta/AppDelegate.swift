09fe23d90cbb8f09112bce9dd6e223b08cced9f2
Johannes Sörensen <johannes.sorensen@ingka.ikea.com>
2021-03-28 12:36:56 +0200

    Initial Commit

---
+++ Vakta/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Vakta
+//
+//  Created by Johannes Sörensen on 2021-03-28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
